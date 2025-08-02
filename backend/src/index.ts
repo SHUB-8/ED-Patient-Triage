052c79164585746d94bfa677d0f5fc2b77ddf434
Prajwal Mandlecha <prajwalmandlecha@gmail.com>
2025-08-03 03:12:42 +0530

    basic intialization changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,32 @@
 import express from "express";
+import prisma from "./lib/prisma";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.use("/api");
+
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  console.log("");
+  server.close(() => {
+    console.log("Server closed and Prisma client disconnected");
+    process.exit(0);
+  });
+});
+
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  server.close(() => {
+    console.log("Server closed and Prisma client disconnected");
+    process.exit(0);
+  });
 });
